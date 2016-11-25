4326876181a39af20f6a38692609e7d84001e890
Joe Spedale <jspedale@gmail.com>
2016-11-25 00:01:20 -0500

    Added a search field. When button is clicked, a get request is sent to Wikipedia containing the text in the textbox

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 $(document).ready(function(){
 
-var searched = document.getElementById('searchbox').value;
+var textValue;
 
-    $.ajax({
-        type: "GET",
-        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
-        contentType: "application/json; charset=utf-8",
-        async: false,
-        dataType: "json",
-        success: function (data, textStatus, jqXHR) {
-            console.log(data);
-            var markup = data.parse.text["*"];
-            var blurb = $('<div></div>').html(markup);
-            $('#article').html($(blurb).find('p'));
-        },
-        error: function (errorMessage) {
-        }
-    });
+$('#text_val').click(function() {
+  event.preventDefault();
+  textValue = $('input').val();
+  if (textValue === '') {
+    console.log('Enter text!!');
+  }
+  else {
+    console.log(textValue);
+    // $.ajax({
+    //     type: "GET",
+    //     url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+ textValue + "&callback=?",
+    //     contentType: "application/json; charset=utf-8",
+    //     async: false,
+    //     dataType: "json",
+    //     success: function (data, textStatus, jqXHR) {
+    //         console.log(data);
+    //         var markup = data.parse.text["*"];
+    //         var blurb = $('<div></div>').html(markup);
+    //         $('#article').html($(blurb).find('p'));
+    //     },
+    //     error: function (errorMessage) {
+    //     }
+    // });
+
+    $.getJSON("http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+ textValue + "&callback=?",
+      function(json) {
+        console.log(json.parse.text['*']);
+      });
+  }
+});
 });
