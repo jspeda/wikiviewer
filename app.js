5ca4007a82924db685cff3a5f76fcec74be0dfed
Joe Spedale <jspedale@gmail.com>
2016-11-28 16:36:49 -0500

    Fixed title link generation to deal with titles with spaces and added Pure css

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ $('#text_val').click(function() {
       console.log(json.query.search);
       // loop through json to populate page with content
       for (var i = 0; i < search.length; i++) {
-        var title = $("<div class='result' id='title'><a href=https://en.wikipedia.org/wiki/" + search[i].title + ">" + search[i].title + "</a></div>");
+        var urlTitle = search[i].title.replace(/ /g, "_");
+        var title = $("<div class='result' id='title'><a href=https://en.wikipedia.org/wiki/" + urlTitle + ">" + search[i].title + "</a></div>");
         var snippet = $("<div class='result' id='snippet'>" + search[i].snippet + "</div>");
         $('.results-container').append(title);
         for (var j = 0; j < search.length; j++) {
