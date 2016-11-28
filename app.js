d55e9a21dde60637dd6f19a746a2aec8631a65a7
Joe Spedale <jspedale@gmail.com>
2016-11-28 17:51:54 -0500

    Added formatting to generated divs

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ $('#text_val').click(function() {
       // loop through json to populate page with content
       for (var i = 0; i < search.length; i++) {
         var urlTitle = search[i].title.replace(/ /g, "_");
-        var title = $("<div class='result' id='title'><a href=https://en.wikipedia.org/wiki/" + urlTitle + ">" + search[i].title + "</a></div>");
-        var snippet = $("<div class='result' id='snippet'>" + search[i].snippet + "</div>");
+        var title = $("<div class='result result-title pure-u-1-2' id='title'><a href=https://en.wikipedia.org/wiki/" + urlTitle + ">" + search[i].title + "</a></div>");
+        var snippet = $("<div class='result result-snippet pure-u-1' id='snippet'>" + search[i].snippet + "</div><br />");
         $('.results-container').append(title);
         for (var j = 0; j < search.length; j++) {
           $('.results-container').append(snippet);
