6844f7c9ac6d3e8d64dff6a8eaeafc42ea55c469
Joe Spedale <jspedale@gmail.com>
2016-11-28 21:13:55 -0500

    Searching clears the existing content

--- app.js
+++ app.js
@@ -9,24 +9,9 @@ $('#text_val').click(function() {
     console.log('Enter text!!');
   }
   else {
+    $('.results-title-container').html('');
+    $('.results-container').html('');
     console.log(textValue);
-    // $.ajax({
-    //     type: "GET",
-    //     url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+ textValue + "&callback=?",
-    //     contentType: "application/json; charset=utf-8",
-    //     async: false,
-    //     dataType: "json",
-    //     success: function (data, textStatus, jqXHR) {
-    //         console.log(data);
-    //         var markup = data.parse.text["*"];
-    //         var blurb = $('<div></div>').html(markup);
-    //         $('#article').html($(blurb).find('p'));
-    //     },
-    //     error: function (errorMessage) {
-    //     }
-    // });
-
-  // this one works
   $.getJSON("https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch="+ textValue + "&format=json&callback=?",
     function(json) {
       var search = json.query.search;
