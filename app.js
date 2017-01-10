17a23e429423d3a2828ce00dd886b6dbe66e4fad
Joe Spedale <jspedale@gmail.com>
2017-01-09 23:22:49 -0500

    Fixed setTimeout and some mild refactoring.

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 $(document).ready(function(){
-$('form').on('keyup', function(event) {
-  let textValue = $('input').val();
-  if (textValue === '') {
-    $('.results-container').html('');
-  }
-  else {
-    setTimeout(getResults, 400);
-    function getResults() {
-      $('.results-title-container').html('');
+
+  $('form').on('keyup', function(event) {
+    let textValue = $('input').val();
+    if (textValue === '') {
       $('.results-container').html('');
-      $.getJSON(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=
-      ${textValue}&format=json&callback=?`,
-      function(json) {
-        const search = json.query.search;
-        console.log(json.query.search);
-        search.map(function(result) {
-          const urlTitle = result.title.replace(/\s/g, "_");
-          const title = $(`<div class='result result-title pure-u-1-2' id='title'>
-            <a href=https://en.wikipedia.org/wiki/${urlTitle}>${result.title}</a></div>`);
-          const snippet = $(`<div class='result result-snippet pure-u-1' id='snippet'>
-            ${result.snippet}</div><br />`);
-          return $('.results-container').append(title).append(snippet);
-        });
-      });
     }
+    else {
+      setTimeout(function() {
+        getResults(textValue);
+      }, 150);
+    }
+  });
+
+  function getResults(searchTerm) {
+    $('.results-title-container').html('');
+    $('.results-container').html('');
+    $.getJSON(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=
+    ${searchTerm}&format=json&callback=?`,
+    function(json) {
+      const search = json.query.search;
+      console.log(json.query.search);
+      search.map(function(result) {
+        const urlTitle = result.title.replace(/\s/g, "_");
+        const title = $(`<div class='result result-title pure-u-1-2' id='title'>
+          <a href=https://en.wikipedia.org/wiki/${urlTitle}>${result.title}</a></div>`);
+        const snippet = $(`<div class='result result-snippet pure-u-1' id='snippet'>
+          ${result.snippet}</div><br />`);
+        return $('.results-container').append(title).append(snippet);
+      });
+    });
   }
-});
 });
